423ca4a0cea81ef7232fa7bed72d57b11f3c29b6
Guido Duchi <gduchi@localhost.localdomain>
2018-01-05 11:53:41 -0500

    interface completa

---
+++ src/main/java/operaciones.java
@@ -0,0 +1,20 @@
+public class operaciones {
+	//suma de dos numeros
+	public int sum (int a,int b) {
+		return a + b;
+	}
+	public int mul (int a,int b) {
+		return a * b;
+	}
+	public int div (int a,int b) {
+		return a / b;
+	}
+	public int res (int a,int b) {
+		return a - b;
+	}
+	
+	public operaciones(){
+		int a=0;
+		int b=0;
+	}
+}
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -4,6 +4,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
